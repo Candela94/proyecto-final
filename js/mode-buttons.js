f9731301ba1fe657a5307582a21a08d6d3ef1e46
Candela94 <candelafsg@alumnos.cei.es>
2024-12-09 08:29:19 +0100

    dia3

---
+++ js/mode-buttons.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+//Selección del botón para agregarle una función
+const modeButton = document.querySelector("#modeButton");
+
+
+    modeButton.addEventListener("click", () =>{
+
+        modeButton.classList.add('u-darkMode');
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
